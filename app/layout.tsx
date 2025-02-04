c2a030f91565d121ebf5d1f136c9e3abcdd14659
Yash Garg <yashengg1512@gmail.com>
2025-02-04 22:43:46 +0530

    added UserProvider and SupabaseProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
 
 const font = Figtree({
   subsets: ["latin"],
@@ -22,9 +24,13 @@ export default function RootLayout({
       <body
         className={`${font.className} antialiased`}
       >
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
